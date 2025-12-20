a5e08ae48ae177fe787e7de3d761267874d60106
Kevin Ferreira <kevincristhian@outlook.com>
2025-12-20 09:40:39 -0300

    refactor: added businnes rule for description

--- src/core/entities/category.ts
+++ src/core/entities/category.ts
@@ -4,43 +4,41 @@ import { CategoryProps } from "../props";
 import { Name, Color } from "../value-objects/category";
 
 export class Category {
-    private constructor(
-        private readonly _name: Name,
-        private readonly _color: Color,
-        private readonly _description?: string,
-        private readonly _id?: UniqueNumericId
-    ) { }
+	private constructor(
+		private readonly _name: Name,
+		private readonly _color: Color,
+		private readonly _description?: string,
+		private readonly _id?: UniqueNumericId,
+	) {}
 
-    public static create(props: CategoryProps): Category {
-        const { name, color, description, id } = props;
-        const nameInstance = Name.create(name);
-        const colorInstance = Color.create(color);
-        const uniqueId = id ? UniqueNumericId.create(id) : UniqueNumericId.create();
-        if (description && description.length > 255) {
-            throw new BusinessRuleViolationError("Descrição não pode exceder 255 caracteres", 422);
-        }
+	public static create(props: CategoryProps): Category {
+		const { name, color, description, id } = props;
+		const nameInstance = Name.create(name);
+		const colorInstance = Color.create(color);
+		const uniqueId = id ? UniqueNumericId.create(id) : UniqueNumericId.create();
+		if (description && description.length > 255) {
+			throw new BusinessRuleViolationError(
+				"Descrição não pode exceder 255 caracteres",
+				422,
+			);
+		}
 
-        return new Category(
-            nameInstance,
-            colorInstance,
-            description,
-            uniqueId
-        );
-    }
+		return new Category(nameInstance, colorInstance, description, uniqueId);
+	}
 
-    public get id(): UniqueNumericId {
-        return this._id
-    }
+	public get id(): UniqueNumericId {
+		return this._id;
+	}
 
-    public get name(): Name {
-        return this._name
-    }
+	public get name(): Name {
+		return this._name;
+	}
 
-    public get color(): Color {
-        return this._color
-    }
+	public get color(): Color {
+		return this._color;
+	}
 
-    public get description(): string {
-        return this._description
-    }
+	public get description(): string {
+		return this._description;
+	}
 }
