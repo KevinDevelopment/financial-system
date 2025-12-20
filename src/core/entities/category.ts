0d4c3b118774bb09a630d9e86d13517901abacff
Kevin Ferreira <kevincristhian@outlook.com>
2025-12-20 09:20:54 -0300

    feat: added category entity

--- src/core/entities/category.ts
+++ src/core/entities/category.ts
@@ -1,6 +1,46 @@
+import { UniqueNumericId } from "../value-objects/unique-numeric-id";
+import { BusinessRuleViolationError } from "../exception";
+import { CategoryProps } from "../props";
+import { Name, Color } from "../value-objects/category";
+
 export class Category {
     private constructor(
-        private readonly _name: string,
-        
-    ) {}
+        private readonly _name: Name,
+        private readonly _color: Color,
+        private readonly _description?: string,
+        private readonly _id?: UniqueNumericId
+    ) { }
+
+    public static create(props: CategoryProps): Category {
+        const { name, color, description, id } = props;
+        const nameInstance = Name.create(name);
+        const colorInstance = Color.create(color);
+        const uniqueId = id ? UniqueNumericId.create(id) : UniqueNumericId.create();
+        if (description && description.length > 255) {
+            throw new BusinessRuleViolationError("Descrição não pode exceder 255 caracteres", 422);
+        }
+
+        return new Category(
+            nameInstance,
+            colorInstance,
+            description,
+            uniqueId
+        );
+    }
+
+    public get id(): UniqueNumericId {
+        return this._id
+    }
+
+    public get name(): Name {
+        return this._name
+    }
+
+    public get color(): Color {
+        return this._color
+    }
+
+    public get description(): string {
+        return this._description
+    }
 }
---
+++ src/core/props/category-props.ts
@@ -0,0 +1,6 @@
+export interface CategoryProps {
+    name: string;
+    color: string;
+    description?: string;
+    id?: bigint;
+}
