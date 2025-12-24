c1427363c1aeebc183d99addce34b1449cc286b8
Kevin Ferreira <kevincristhian@outlook.com>
2025-12-24 11:20:23 -0300

    refactor: category routes

--- src/presentation/routes/category-routes.ts
+++ src/presentation/routes/category-routes.ts
@@ -2,15 +2,9 @@ import { FastifyInstance } from "fastify";
 import { makeRouteHandler, makeCreateCategoryController } from "../factories";
 import { CreateCategoryControllerAdapter } from "../route-adapters";
 
-<<<<<<< HEAD
-export async function organizationRoutes(fastify: FastifyInstance) {
-	fastify.post(
-		"/v1/organizations",
-=======
 export async function categoryRoutes(fastify: FastifyInstance) {
 	fastify.post(
 		"/v1/categories",
->>>>>>> e2238ee (feat: added create category flow)
 		makeRouteHandler(
 			CreateCategoryControllerAdapter,
 			makeCreateCategoryController,
