c72895e001d2dc56f055883e33d1d6f485bb0aae
Kevin Ferreira <kevincristhian@outlook.com>
2026-01-17 09:59:46 -0300

    feat: add verify token use case and DTOs

---
+++ src/core/application/use-cases/verify-token-use-case.ts
@@ -0,0 +1,35 @@
+import { TokenService } from "../services";
+import { VerifyTokenInputDto, VerifyTokenOutputDto } from "../dto";
+import { MissingDataError, InvalidValueError } from "../../domain/errors";
+
+export class VerifyTokenUseCase {
+    constructor(
+        private readonly tokenService: TokenService
+    ) { }
+
+    async perform(input: VerifyTokenInputDto): Promise<VerifyTokenOutputDto> {
+        if (!input.token) {
+            throw new MissingDataError("Token de acesso não informado", 401)
+        }
+
+        if (typeof input.token !== "string") {
+            throw new InvalidValueError("Token de acesso inválido", 401)
+        }
+
+        if (input.token.match(/\s/)) {
+            throw new InvalidValueError("Token de acesso inválido", 401)
+        }
+
+        try {
+            const { sub, organizationId, role } = await this.tokenService.verify<VerifyTokenOutputDto>("access", input.token);
+
+            return {
+                sub,
+                organizationId,
+                role
+            }
+        } catch (error) {
+            throw new InvalidValueError("Token inválido ou expirado", 401)
+        }
+    }
+}
