905e1d481f8240668d0e6211c465f27b5921c453
Kevin Ferreira <kevincristhian@outlook.com>
2026-01-18 10:12:00 -0300

    refactor: removed magic numbers

--- src/core/domain/value-objects/global/name.ts
+++ src/core/domain/value-objects/global/name.ts
@@ -14,9 +14,11 @@ export class Name {
 	}
 
 	private static isValid(value: string): void {
+		const MAX_QUANTITY_OF_PERMITTED_CHARACTERS = 100;
+
 		if (!value)
 			throw new BusinessRuleViolationError("Nome não pode ser vazio", 422);
-		if (value.length < 2 || value.length > 100) {
+		if (value.length < 2 || value.length > MAX_QUANTITY_OF_PERMITTED_CHARACTERS) {
 			throw new BusinessRuleViolationError(
 				"Nome deve ter entre 2 e 100 caracteres",
 				422,
