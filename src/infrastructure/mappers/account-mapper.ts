2e494915d41c179558374ac72db8a0c75f434361
Kevin Ferreira <kevincristhian@outlook.com>
2026-01-18 09:31:09 -0300

    refactor: account mapper

--- src/infrastructure/mappers/account-mapper.ts
+++ src/infrastructure/mappers/account-mapper.ts
@@ -1,27 +1,27 @@
 import { Account } from "../../core/domain/entities/account";
 
 export const accountMapper = {
-	toPersistence(account: Account) {
-		return {
-			id: account.id.value,
-			name: account.name.value,
-			type: account.type.value,
-			initialBalance: account.initialBalance.toCents(),
-			currentBalance: account.currentBalance.toCents(),
-			organizationId: account.organizationId.value,
-			userId: account.userId.value,
-		};
-	},
+    toPersistence(account: Account) {
+        return {
+            id: account.id.value,
+            name: account.name.value,
+            type: account.type.value,
+            initialBalance: account.initialBalance.toCents(),
+            currentBalance: account.currentBalance.toCents(),
+            organizationId: account.organizationId.value,
+            userId: account.userId.value,
+        };
+    },
 
-	toDomain(row: any): Account {
-		return Account.create({
-			id: row.id,
-			name: row.name,
-			type: row.type,
-			initialBalance: row.initialBalance,
-			currentBalance: row.currentBalance,
-			organizationId: row.organizationId,
-			userId: row.userId,
-		});
-	},
+    toDomain(row: any): Account {
+        return Account.create({
+            id: row.id,
+            name: row.name,
+            type: row.type,
+            initialBalance: row.initialBalance,
+            currentBalance: row.currentBalance,
+            organizationId: row.organizationId,
+            userId: row.userId,
+        });
+    },
 };
